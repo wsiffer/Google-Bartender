dc790410ac1af55ddb50a51ceb4507e6fb176142
wsiffer <willsiffer@ymail.com>
2020-06-24 22:08:41 -0400

    minor changes

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from flask import Flask, request, Response
 from bartender import Bartender
 from drinks import drink_list, drink_options
 
-bartender = Bartender()
+#bartender = Bartender()
 bartender.buildMenu(drink_list, drink_options)
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def respond():
-    print(request.data);
+    print(request.data)
+    print(menuItem.name)
     return Response(status=200)
 
