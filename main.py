4174c0a3c7d7b78d03ccfaa57011885967d8cf99
wsiffer <willsiffer@ymail.com>
2020-06-24 21:24:38 -0400

    initial flask server to ref. bartender.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from flask import Flask, request, Response
+from bartender import Bartender
+from drinks import drink_list, drink_options
+
+bartender = Bartender()
+bartender.buildMenu(drink_list, drink_options)
 
 app = Flask(__name__)
 
