4ba0fc3972c1f760557b846d90cc5b1a274c1cd0
wsiffer <willsiffer@ymail.com>
2020-06-24 22:32:47 -0400

    updating remote repo

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from flask import Flask, request, Response
 from bartender import Bartender
 from drinks import drink_list, drink_options
 from menu import MenuItem, Menu, Back, MenuContext, MenuDelegate
-
 bartender = Bartender()
 bartender.buildMenu(drink_list, drink_options)
 
+
+
 app = Flask(__name__)
+
 
 @app.route('/webhook', methods=['POST'])
 def respond():
     print(request.data)
-    print(MenuItem.name)
     return Response(status=200)
 
