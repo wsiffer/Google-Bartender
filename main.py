cf527cb42a68bcb957e39c80d87185784b20b8b9
wsiffer <willsiffer@ymail.com>
2020-07-06 11:54:20 -0400

    real progress made

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from menu import MenuItem, Menu, Back, MenuContext, MenuDelegate
 bartender = Bartender()
 bartender.buildMenu(drink_list, drink_options)
 
-app = Flask(__name__)
-menuItem = MenuItem()
 
+app = Flask(__name__)
+
+bartender.menuContext.select()
+#bartender.menuContext.select()
+#bartender.menuContext.select()
 
 @app.route('/webhook', methods=['POST'])
 def respond():
