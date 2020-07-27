fd3094baa3550c2e325eeae0d85ecae9250c7ffa
WILL SIFFER <willsiffer@ymail.com>
2020-07-27 14:19:12 -0400

    working - adding cleaning function

--- main.py
+++ main.py
@@ -12,9 +12,24 @@ app = Flask(__name__)
 def respond():
     requestData = str(request.data)[4:].replace("'", "")
 
+    if(requestData == "clean"):
+        while(bartender.screenItem.name != "Configure"):
+            pete.menuContext.advance()
+        pete.menuContext.select()
+        while(bartender.screenItem.name != "Clean"):
+            pete.menuContext.advance()
+        pete.menuContext.select()
+
+        for i in range(0,2):
+            while(bartender.screenItem.name != "Back"):
+                pete.menuContext.advance()
+            pete.menuContext.select()
+
+    return Response(status=200)
+
     i = 0
     while(requestData != bartender.screenItem.name):
-        if(i == 1):
+        if(i == 2):
             break
 
         pete.menuContext.advance()
