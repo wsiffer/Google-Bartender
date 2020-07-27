2b53ba1e4bf6f2c0741d98b5e2fdca6d8481a984
WILL SIFFER <willsiffer@ymail.com>
2020-07-27 09:06:34 -0400

    webserver is able to make every drink, functional commit

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
+import bartender
 from flask import Flask, request, Response
-from bartender import Bartender, screenItem
+#from bartender import Bartender, screenItem
 from drinks import drink_list, drink_options
 from menu import MenuItem, Menu, Back, MenuContext, MenuDelegate
 
-bartender = Bartender()
-bartender.buildMenu(drink_list, drink_options)
-
+pete = bartender.Bartender()
+pete.buildMenu(drink_list, drink_options)
+#print("FIRST" + bartender.screenItem.name)
+#pete.menuContext.advance()
+#print("SECOND" + bartender.screenItem.name)
 
 app = Flask(__name__)
 
 @app.route('/webhook/', methods=['POST'])
 def respond():
     requestData = str(request.data)[4:].replace("'", "")
-    menuItem = str(screenItem.name)
+    menuItem = str(bartender.screenItem.name)
     print('request: ' + requestData + ":")
     print(menuItem)
 
-    while(requestData != menuItem):
-        bartender.menuContext.advance()
+    while(requestData != bartender.screenItem.name):
+        pete.menuContext.advance()
         print("REQUEST: " + requestData)
-        print("MENU: " + screenItem.name)
+        print("MENU: " + bartender.screenItem.name)
+
+    if(requestData == bartender.screenItem.name):
+        pete.menuContext.select()
 
     return Response(status=200)
 
