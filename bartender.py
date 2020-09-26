0025b5e5a74ebd4d6ddf5c81c14dc052ade5ac80
WILL SIFFER <willsiffer@ymail.com>
2020-09-26 14:19:22 -0400

    lighttest

--- bartender.py
+++ bartender.py
@@ -7,9 +7,9 @@ import RPi.GPIO as GPIO
 import json
 import threading
 import traceback
+import board
+import neopixel
 
-
-#from dotstar import Adafruit_DotStar
 from menu import MenuItem, Menu, Back, MenuContext, MenuDelegate
 from drinks import drink_list, drink_options
 
@@ -27,13 +27,12 @@ RIGHT_PIN_BOUNCE = 2000
 OLED_RESET_PIN = 15
 OLED_DC_PIN = 16
 
-NUMBER_NEOPIXELS = 45
-NEOPIXEL_DATA_PIN = 26
-NEOPIXEL_CLOCK_PIN = 6
-NEOPIXEL_BRIGHTNESS = 64
+num_pixels = 45
+ORDER = neopixel.GRB
 
 FLOW_RATE = 50.0/100.0
 screenItem = 'IU-SUCKS'
+pixel_pin = board.D18
 
 class Bartender(MenuDelegate): 
     def __init__(self):
@@ -72,7 +71,7 @@ class Bartender(MenuDelegate):
             GPIO.setup(self.pump_configuration[pump]["pin"], GPIO.OUT, initial=GPIO.HIGH)
 
         # setup pixels:
-        #self.numpixels = NUMBER_NEOPIXELS # Number of LEDs in strip
+        pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
 
         # Here's how to control the strip from any two GPIO pins:
         #datapin  = NEOPIXEL_DATA_PIN
@@ -82,6 +81,8 @@ class Bartender(MenuDelegate):
         #self.strip.setBrightness(NEOPIXEL_BRIGHTNESS) # Limit brightness to ~1/4 duty cycle
 
         # turn everything off
+        pixels.fill((255,0,0))
+        pixels.show()
         #for i in range(0, self.numpixels):
            # self.strip.setPixelColor(i, 0)
         #self.strip.show()
@@ -241,32 +242,30 @@ class Bartender(MenuDelegate):
         color = 0xFF0000        # 'On' color (starts red)
 
         while getattr(t, "do_run", True):
-            #self.strip.setPixelColor(head, color) # Turn on 'head' pixel
-            #self.strip.setPixelColor(tail, 0)     # Turn off 'tail'
-            #self.strip.show()                     # Refresh strip
+            pixels[head] = color # Turn on 'head' pixel
+            pixels[tail] = (0, 0, 0)     # Turn off 'tail'
+            pixels.show()                     # Refresh strip
             time.sleep(1.0 / 50)             # Pause 20 milliseconds (~50 fps)
 
             head += 1                        # Advance head position
-            #if(head >= self.numpixels):           # Off end of strip?
-                #head    = 0              # Reset to start
-                #color >>= 8              # Red->green->blue->black
-                #(color == 0): color = 0xFF0000 # If black, reset to red
+            if(head >= num_pixels):           # Off end of strip?
+                head    = 0              # Reset to start
+                color >>= 8              # Red->green->blue->black
+                if(color == 0): color = 0xFF0000 # If black, reset to red
 
             tail += 1                        # Advance tail position
-            #if(tail >= self.numpixels): tail = 0  # Off end? Reset
+            if(tail >= num_pixels): tail = 0  # Off end? Reset
 
     def lightsEndingSequence(self):
         # make lights green
-        #for i in range(0, self.numpixels):
-            #self.strip.setPixelColor(i, 0xFF0000)
-        #self.strip.show()
+        pixels.fill((0, 255, 0))
+        self.strip.show()
 
         time.sleep(5)
 
         # turn lights off
-        #for i in range(0, self.numpixels):
-            #self.strip.setPixelColor(i, 0)
-        #self.strip.show()
+        pixels.fill((0, 0, 0))
+        pixels.show()
 
     def pour(self, pin, waitTime):
         GPIO.output(pin, GPIO.LOW)
