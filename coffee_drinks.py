c54bc73dd7dd179c09bbbc0110f64b680b50f06a
wsiffer <willsiffer@ymail.com>
2021-01-24 15:57:12 -0500

    added coffee drinks

---
+++ coffee_drinks.py
@@ -0,0 +1,42 @@
+# drinks.py
+drink_list = [
+	{
+		"name": "vanilla cold brew",
+		"ingredients": {
+			"van": 10,
+			"cbrew": 300
+		}
+	}, {
+		"name": "vanilla cream cold brew",
+		"ingredients": {
+			"van": 10,
+			"cream": 20,
+			"cbrew": 300
+		}
+	}, {
+		"name": "caramel cold brew",
+		"ingredients": {
+			"car": 10,
+			"cbrew": 300
+		}
+	},{
+		"name": "caramel cream cold brew",
+		"ingredients": {
+			"car": 10,
+			"cream": 20,
+			"cbrew": 300
+		}
+	}, {
+		"name": "black cold brew",
+		"ingredients": {
+			"cbrew": 310
+		}
+	}
+]
+
+drink_options = [
+	{"name": "Cold Brew", "value": "cbrew"},
+	{"name": "Vanilla", "value": "van"},
+	{"name": "Cream", "value": "cream"},
+	{"name": "Caramel", "value": "car"}
+]
