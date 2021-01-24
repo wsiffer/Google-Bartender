b8db28427212396248d2892160a1c85256a12373
wsiffer <willsiffer@ymail.com>
2021-01-24 15:59:26 -0500

    added coffee drinks

--- drinks.py
+++ drinks.py
@@ -90,6 +90,37 @@ drink_list = [
 		"ingredients": {
 			"rum": 50
 		}
+	},{
+		"name": "vanilla cold brew",
+		"ingredients": {
+			"van": 10,
+			"cbrew": 300
+		}
+	}, {
+		"name": "vanilla cream cold brew",
+		"ingredients": {
+			"van": 10,
+			"cream": 20,
+			"cbrew": 300
+		}
+	}, {
+		"name": "caramel cold brew",
+		"ingredients": {
+			"car": 10,
+			"cbrew": 300
+		}
+	},{
+		"name": "caramel cream cold brew",
+		"ingredients": {
+			"car": 10,
+			"cream": 20,
+			"cbrew": 300
+		}
+	}, {
+		"name": "black cold brew",
+		"ingredients": {
+			"cbrew": 310
+		}
 	}
 ]
 
@@ -108,5 +139,9 @@ drink_options = [
 	{"name": "Strawberry Vodka", "value": "svodka"},
 	{"name": "Malibu", "value": "malibu"},
 	{"name": "Grenadine", "value": "gren"},
-	{"name": "Cranberry", "value": "cran"}
+	{"name": "Cranberry", "value": "cran"},
+	{"name": "Cold Brew", "value": "cbrew"},
+	{"name": "Vanilla", "value": "van"},
+	{"name": "Cream", "value": "cream"},
+	{"name": "Caramel", "value": "car"}
 ]
